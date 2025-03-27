a8f47f3d446cfc91971533fdb06a5c66eab08a92
Brian <brian.kiprono8@student.moringaschool.com>
2025-03-27 04:06:14 +0300

    simlified fetch

--- Index.js
+++ Index.js
@@ -1,18 +1,11 @@
-document.addEventListener ("DOMContentLoaded",fetchingAnime)
-function fetchingAnime(){
+let animeData = {}; // Store anime data
+document.addEventListener("DOMContentLoaded", function () {
     fetch("http://localhost:3000/anime") 
-    .then(responce=>responce.json())
-    .then(data=>{
-        const genre=document.getElementById("genre")
-        genre.innerHTML="";
-        data.forEach(anime => {
-            
-            const span =document.createElement("span")
-            span.textContent=anime.name;
-            span.style.cursor="pointer"
-            span.addEventListener("click",()=>showAnimeDescription(anime));
-            genre.appendChild(span)
-        });
-    })
-    .catch(error=>console.log("Anime not fetched:",error));
-}
+        .then(response => response.json()) 
+        .then(data => {
+            animeData = data.anime;  
+        })
+        .catch(error => console.log("Error fetching anime:", error))
+})
+ 
+
