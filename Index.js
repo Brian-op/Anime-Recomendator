cb1e74b5117bd723cf03a928ea872c024020de0c
Brian <brian.kiprono8@student.moringaschool.com>
2025-03-27 06:48:13 +0300

    showing genres

--- Index.js
+++ Index.js
@@ -1,13 +1,30 @@
-let animeData = {}; // Store anime data
-document.addEventListener("DOMContentLoaded", function () {
+// Store anime data
+document.addEventListener("DOMContentLoaded",()=>{
+    fetchAnimeData()
+})
+let animeData = {}; 
+function fetchAnimeData() {
     fetch("http://localhost:3000/anime") 
         .then(response => response.json()) 
         .then(data => {
-            animeData = data.anime;  
+            animeData = data.anime;
+            showGenres(Object.keys(animeData))  
         })
-        .catch(error => console.log("Error fetching anime:", error))
-})
+        .catch(error => console.error("Error fetching anime:", error))
+}
  
+function  showGenres(genres){
+    const genreContainer= document.getElementById("genreContainer")
+    genreContainer.innerHTML="";
+    genres.forEach(genre => {
+        const button=document.createElement("button")
+        button.textContent=genre;
+        button.classList.add("genreButton")
+        button.addEventListener("click",()=>showAnimeTitles(genre))
+        genreContainer.appendChild (button)
+        
+    });
+}
 
 document.getElementById ("watchlist").addEventListener("submit",(event)=>{
     event.preventDefault()
