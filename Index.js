b63d0632f77835559a66c9efea8b5c9ae9fa7590
Brian <brian.kiprono8@student.moringaschool.com>
2025-03-27 03:47:59 +0300

    fetching complete

--- Index.js
+++ Index.js
@@ -1,4 +1,18 @@
 document.addEventListener ("DOMContentLoaded",fetchingAnime)
 function fetchingAnime(){
-    
+    fetch("http://localhost:3000/anime") 
+    .then(responce=>responce.json())
+    .then(data=>{
+        const genre=document.getElementById("genre")
+        genre.innerHTML="";
+        data.forEach(anime => {
+            
+            const span =document.createElement("span")
+            span.textContent=anime.name;
+            span.style.cursor="pointer"
+            span.addEventListener("click",()=>showAnimeDescription(anime));
+            genre.appendChild(span)
+        });
+    })
+    .catch(error=>console.log("Anime not fetched:",error));
 }
